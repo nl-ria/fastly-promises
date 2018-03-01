64670ff1d8887a469b07a26bd86e3da0de906cd4
Lohia,Nikita <nik1389@gmail.com>
2018-03-01 10:05:21 +0000

    Trying to add pagination-2.0

---
+++ test/getWafRulesByTags.test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const nock = require('nock');
+const expect = require('expect');
+const config = require('../src/config');
+const fastlyPromises = require('../src/index');
+const response = require('./response/getWafRulesByTags.response');
+
+describe('#getWafRulesByTags', () => {
+  const fastly = fastlyPromises('923b6bd5266a7f932e41962755bd4254', '79CEhEeP8DKPQQGTXokV5M');
+  let res;
+
+  nock(config.mainEntryPoint)
+    .log(console.log)
+    .get('/service/79CEhEeP8DKPQQGTXokV5M/wafs/rfjn9II8V21LSeEgyMT9x/rule_statuses?filter[rule][tags][name]=language-css&page[number]=1&page[size]=200')
+    .reply(200, response.getWafRulesByTags);
+
+  before(async () => {
+    res = await fastly.getWafRulesByTags('rfjn9II8V21LSeEgyMT9x','language-css');
+  });
+
+  it('response should be a status 200', () => {
+    expect(res.status).toBe(200);
+  });
+
+  it('response body should exist', () => {
+    expect(res.data).toExist();
+  });
+
+  it('response body should be an array', () => {
+    expect(Array.isArray(res.data.data)).toBe(true);
+  });
+
+  it('response should contain WAF ID rfjn9II8V21LSeEgyMT9x-2077876', () => {
+    expect(res.data.data[0].id).toBe("rfjn9II8V21LSeEgyMT9x-2077876");
+  });
+
+  it('response body should contain properties', () => {
+    expect(res.data).toIncludeKeys(['links', 'meta']);
+  });
+
+});
